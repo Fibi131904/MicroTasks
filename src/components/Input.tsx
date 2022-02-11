83cef921a8ced0f5982f3f9ffc6ddba18a499268
Kseniya Smirnova <fanni@bk.ru>
2022-02-11 22:42:13 +0300

    2

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,6 +6,7 @@ type InputPropsType ={
 }
 
 export const Input=(props: InputPropsType)=> {
+    
     const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
         props.setTitle(event.currentTarget.value)
       }
